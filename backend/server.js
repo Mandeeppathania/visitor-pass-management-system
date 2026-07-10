f94295ac7592d16b8e4fffc36b737e10c1daba84
Mandeep Singh <mandeep04pathania@gmail.com>
2026-07-11 02:46:10 +0530

    Created User Model

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
 
-const PORT = process.env.PORT || 5000;
-
+// Connect to database
 connectDB();
 
+const PORT = process.env.PORT || 5000;
+
+// Start server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
