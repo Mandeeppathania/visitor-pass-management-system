9c040c35875ae6fe6dafe69826b00b1398f89b3f
Mandeep Singh <mandeep04pathania@gmail.com>
2026-07-11 02:58:43 +0530

    Created Appointment model

---
+++ backend/models/Appointment.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const appointmentSchema = new mongoose.Schema(
+{
+    visitor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Visitor",
+        required: true
+    },
+
+    host: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    visitDate: {
+        type: Date,
+        required: true
+    },
+
+    purpose: {
+        type: String,
+        required: true
+    },
+
+    status: {
+        type: String,
+        enum: ["pending", "approved", "rejected"],
+        default: "pending"
+    }
+},
+{
+    timestamps: true
+}
+);
+
+module.exports = mongoose.model("Appointment", appointmentSchema);
