238550eaa6017051e620e816e92a66f6c1c5b4ff
Mandeep Singh <mandeep04pathania@gmail.com>
2026-07-11 19:42:09 +0530

    Created controller for Authentication

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,63 @@
-const login = (req, res) => {
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-    res.send("Login API Working");
+// Login
+const login = async (req, res) => {
+
+    try {
+
+        // Get email and password from frontend
+        const { email, password } = req.body;
+
+        // Check if user exists
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        // Compare password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                message: "Invalid password"
+            });
+        }
+
+        // Create JWT Token
+        const token = jwt.sign(
+            {
+                id: user._id,
+                role: user.role
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1d"
+            }
+        );
+
+        return res.status(200).json({
+    message: "Login Successful",
+    token,
+    user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+    }
+});
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
 
 };
 
